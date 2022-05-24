4f245d94d9974af71aa13abf97d4f583a0dce3f7
Luis Vitor <luis.vitor@dcx.ufpb.br>
2022-05-24 15:08:09 -0300

    commitando parte 5 da atividade pratica

---
+++ LuisVitorAraujoDeAmorim.java
@@ -0,0 +1,7 @@
+public class LuisVitorAraujoDeAmorim {
+
+    public void main (String[] args) {
+        System.out.println("Luis Vitor Araujo de Amorim");
+        System.out.println("Matricula: 20180031717");
+    }
+}
