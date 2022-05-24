a32d96c89bafc8c6b423a631eca5e90a392e1633
joseivo01 <jose.lima@dcx.ufpb.br>
2022-05-24 15:03:33 -0300

    primeiro commit

---
+++ hello_jose_ivo.java
@@ -0,0 +1,7 @@
+public class hello_jose_ivo {
+    public static void main(String[] args) {
+        System.out.println("José Ivo de lima sobrinho de \n"
+                           +"RG: 709.027.XXX-XX \n"
+                           +"É isso!");
+    }
+}
